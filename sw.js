54e85deb96a467d7eedd9d80e704f763ac1ddb8a
GRC-CAFE <anotherlife91@gmail.com>
2026-09-12 11:40:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
-const CACHE_NAME = 'grc-cafe-v12';
+const CACHE_NAME = 'grc-cafe-v13';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
